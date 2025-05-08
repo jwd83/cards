883a59c0c64ae645820e5feeb99ae4ea241f2700
Jared De Blander <jared@ayrscott.com>
2025-05-08 18:03:42 -0400

    working on title screen

--- scenes/TitleScene.js
+++ scenes/TitleScene.js
@@ -4,17 +4,23 @@ class TitleScene extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('title', 'images/title.png');
         this.load.image('button', 'images/button-small.png');
     }
 
     create() {
+
+        const titleImage = this.add.image(0, 0, 'title');
+        titleImage.setOrigin(0, 0);
+        titleImage.setScale(0.75);
+
         this.add.text(400, 200, 'The Land of Monsters', {
             fontSize: '32px',
             fill: '#fff'
         }).setOrigin(0.5);
 
         const startButton = this.add.image(400, 350, 'button').setInteractive();
-        startButton.setScale(2);
+        startButton.setScale(1);
 
         this.add.text(400, 350, 'Start Game', {
             fontSize: '20px',
