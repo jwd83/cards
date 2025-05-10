6a119104ebe91f3dbb1bb8d176299fac09202a45
Jared De Blander <jared0x90@gmail.com>
2025-05-10 03:26:21 -0400

    fix: adjust gamepad text position and streamline update logic

--- scenes/GamepadTester.js
+++ scenes/GamepadTester.js
@@ -10,19 +10,15 @@ class GamepadTester extends Phaser.Scene {
             fill: '#fff'
         }).setOrigin(0.5);
 
-        this.gamepadText = this.add.text(1920 / 2, 1080 / 10 * 4, 'No gamepad detected', {
+        this.gamepadText = this.add.text(1920 / 6, 1080 / 10 * 2, 'No gamepad detected', {
             fontSize: '32px',
             fill: '#fff'
-        }).setOrigin(0.5);
-
-
+        }).setOrigin(0);
     }
 
 
-    update ()
-    {
-        if (this.input.gamepad.total === 0)
-        {
+    update() {
+        if (this.input.gamepad.total === 0) {
             this.gamepadText.setText('No gamepad detected');
             return;
         }
@@ -31,20 +27,45 @@ class GamepadTester extends Phaser.Scene {
         var report = [];
 
         report.push('Gamepad ' + pad.index + ': ' + pad.id);
-        if (pad.axes.length)
-        {
+        if (pad.axes.length) {
 
-            var i = 0;
-
-            while (i < pad.axes.length)
-            {
+            for (var i = 0; i < pad.axes.length; i++) {
                 const axis = pad.axes[i].getValue();
                 report.push('Axis ' + i + ': ' + axis.toFixed(3));
-                i++;
             }
-
-
         }
+
+        if (pad.buttons.length) {
+            for (var i = 0; i < pad.buttons.length; i++) {
+                report.push('Button ' + i + ': ' + pad.buttons[i].value);
+            }
+        }
+
+
+        if (pad.up) {
+            report.push('Up pressed');
+        } else {
+            report.push('Up not pressed');
+        }
+
+        if (pad.down) {
+            report.push('Down pressed');
+        } else {
+            report.push('Down not pressed');
+        }
+
+        if (pad.left) {
+            report.push('Left pressed');
+        } else {
+            report.push('Left not pressed');
+        }
+
+        if (pad.right) {
+            report.push('Right pressed');
+        } else {
+            report.push('Right not pressed');
+        }
+
         this.gamepadText.setText(report);
     }
 
