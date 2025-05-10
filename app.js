784ddcc3ae94423184de18ee8cf2c2dc31758991
Jared De Blander <jared@ayrscott.com>
2025-05-10 15:22:50 -0400

    pulled all the css and js out of index.html

---
+++ app.js
@@ -0,0 +1,30 @@
+import Title from "./scenes/Title.js";
+import MainMenu from "./scenes/MainMenu.js";
+import Draft from "./scenes/Draft.js";
+import GamepadTester from "./scenes/GamepadTester.js";
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    loader: {
+        baseURL: "assets/",
+    },
+    input: {
+        gamepad: true,
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 0 },
+            debug: false,
+        },
+    },
+    scene: [Title, GamepadTester, MainMenu, Draft],
+};
+
+const game = new Phaser.Game(config);
+game.pad = null;
