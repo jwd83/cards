8812a6130f1c1bf096ba542003f335a407687f96
Jared De Blander <jared0x90@gmail.com>
2025-05-11 10:35:41 -0400

    refactor: enhance keypad.js with keyboard mapping and improve documentation

--- classes/keypad.js
+++ classes/keypad.js
@@ -6,7 +6,31 @@ single unified input system.
 It has a pressed and held state for each key/button, and it can be used to
 check if a key/button has just been pressed or is being held.
 
+
+Methods to query:
+
+held (key is being held down)
+pressed (key was just pressed this sample)
+released (key was just released this sample)
+duration (how long the key has been held down)
+
+
 */
+
+keyboard_map = {
+    'up': 'W',
+    'down': 'S',
+    'left': 'A',
+    'right': 'D',
+    'A': 'K',
+    'B': 'L',
+    'X': 'J',
+    'Y': 'I',
+    'L1': 'Q',
+    'R1': 'O',
+    'L2': 'Y',
+    'R2': 'P',
+}
 
 function empty_keypad() {
     return {
@@ -29,3 +53,18 @@ function empty_keypad() {
 
     }
 }
+
+class Keypad {
+    constructor(game) {
+        this._game = game;
+        this.p = empty_keypad();
+        this.r = empty_keypad();
+        this.h = empty_keypad();
+        this.d = empty_keypad();
+    }
+
+
+
+}
+
+export default Keypad;
