bbc4add79641d1d103b97b90a582e6dd754ee62f
Jared De Blander <jared@ayrscott.com>
2025-05-13 14:41:33 -0400

    memory game starter and cardback

---
+++ scenes/memory.js
@@ -0,0 +1,47 @@
+export default class MemoryScene extends Phaser.Scene {
+    constructor() {
+        super('MemoryScene');
+    }
+
+    preload() {
+        // Load card back and front assets here
+        this.load.image('cardBack', 'images/cardback.png');
+        for (let i = 1; i <= 4; i++) {
+            this.load.image(`card${i}`, `path/to/card${i}.png`);
+        }
+    }
+
+    create() {
+        // Create a shuffled list of pairs
+        const cardKeys = ['card1', 'card1', 'card2', 'card2', 'card3', 'card3', 'card4', 'card4'];
+        Phaser.Utils.Array.Shuffle(cardKeys);
+
+        this.selectedCards = [];
+        cardKeys.forEach((key, index) => {
+            const x = 100 + (index % 4) * 100;
+            const y = 100 + Math.floor(index / 4) * 140;
+            const card = this.add.image(x, y, 'cardBack').setInteractive();
+            card.frontTexture = key;
+            card.on('pointerup', () => this.flipCard(card));
+        });
+    }
+
+    flipCard(card) {
+        if (this.selectedCards.length < 2 && !this.selectedCards.includes(card)) {
+            card.setTexture(card.frontTexture);
+            this.selectedCards.push(card);
+            if (this.selectedCards.length === 2) {
+                this.time.delayedCall(700, () => this.checkMatch());
+            }
+        }
+    }
+
+    checkMatch() {
+        const [card1, card2] = this.selectedCards;
+        if (card1.frontTexture !== card2.frontTexture) {
+            card1.setTexture('cardBack');
+            card2.setTexture('cardBack');
+        }
+        this.selectedCards = [];
+    }
+}
