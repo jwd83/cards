e08b28b8333263b69cf692f8ee9a4089a09f11d6
Jared De Blander <jared@ayrscott.com>
2025-05-15 01:07:29 -0400

    implement fireball image scaling and positioning in CardInfo

--- scenes/cardinfo.js
+++ scenes/cardinfo.js
@@ -9,6 +9,7 @@ class CardInfo extends Phaser.Scene {
     preload() {
 
         this.load.image('blank', 'images/kayla-blank.png');
+        this.load.image('fireball', 'images/fireball.jpg');
     }
 
     create() {
@@ -22,9 +23,6 @@ class CardInfo extends Phaser.Scene {
         this.#fireball.setOrigin(0, 0);
 
 
-        // Draw the blank background
-        this.#fireball.draw('blank', 0, 0);
-
         /*
 
 
@@ -33,9 +31,64 @@ class CardInfo extends Phaser.Scene {
         top left is 160, 160
         bottom right is 1500, 1060
 
+        width is 1340
+        height is 900
+
+        stretch fireball.jpg to be at least 900 tall and 1340 wide while keeping the aspect ratio
+
+
+        detect the aspect ratio of fireball.jpg and it's width and height
+
 
         */
 
+        // Get the fireball texture dimensions
+        const fireball_width = this.textures.get('fireball').getSourceImage().width;
+        const fireball_height = this.textures.get('fireball').getSourceImage().height;
+
+        // Calculate the aspect ratio
+        const fireball_aspect_ratio = fireball_width / fireball_height;
+
+        // Calculate the new width and height based on the aspect ratio
+        let new_width = 1340;
+        let new_height = 900;
+        if (fireball_aspect_ratio > 1) {
+            // Fireball is wider than it is tall
+            new_height = 900;
+            new_width = fireball_width * (new_height / fireball_height);
+        } else {
+            // Fireball is taller than it is wide
+            new_width = 1340;
+            new_height = fireball_height * (new_width / fireball_width);
+        }
+
+        // make a copy of the fireball image set to the new width and height
+        const fireball_copy = this.add.image(0, 0, 'fireball');
+        fireball_copy.setOrigin(0, 0);
+        fireball_copy.setScale(new_width / fireball_width, new_height / fireball_height);
+
+        // crop the edges to make it exactly 1340 x 900
+        fireball_copy.setCrop(0, 0, 1340, 900);
+        // calculate to crop the center 1340 x 900
+        // const crop_x = (fireball_copy.width - 1340) / 2;
+        // const crop_y = (fireball_copy.height - 900) / 2;
+        // fireball_copy.setCrop(crop_x, crop_y, 1340, 900);
+
+        // set the position of the fireball_copy to be 160, 160
+        // fireball_copy.setX(160);
+        // fireball_copy.setY(160);
+        // // set the origin to be 0, 0
+        // fireball_copy.setOrigin(0, 0);
+
+
+        this.#fireball.draw(fireball_copy, 160, 160)
+        fireball_copy.destroy();
+
+
+
+
+        // Draw the blank background on top of the fireball image
+        this.#fireball.draw('blank', 0, 0);
 
         // Draw the card name
         const card_name = this.add.text(0, 0, 'Fireball', {
@@ -74,8 +127,8 @@ class CardInfo extends Phaser.Scene {
     }
 
     update() {
-        // this.#fireball.setX(Math.sin(this.game._frame_count / 100) * 50 + 50);
-        // this.#fireball.setY(Math.cos(this.game._frame_count / 100) * 50 + 50);
+        this.#fireball.setX(Math.sin(this.game._frame_count / 100) * 50 + 50);
+        this.#fireball.setY(Math.cos(this.game._frame_count / 100) * 50 + 50);
 
     }
 }
