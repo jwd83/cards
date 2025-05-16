9f86349ca4159ac9eb37fa3abd24cae91087774c
Jared De Blander <jared0x90@gmail.com>
2025-05-16 10:55:33 -0400

    porting pygame dragster to js

---
+++ classes/dragster/engine.js
@@ -0,0 +1,17 @@
+export default class Engine {
+    constructor(torque_curve, rpm_shift = 7350, rpm_launch = 1500) {
+
+        this.torque_curve = torque_curve;
+        this.rpm_shift = rpm_shift;
+        this.rpm_launch = rpm_launch;
+
+    }
+
+    torque(rpm) {
+
+    }
+
+    power(rpm) {
+
+    }
+}
