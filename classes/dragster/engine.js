b6d5c407729787500f60b6b52d992008d4c45c38
Jared De Blander <jared@ayrscott.com>
2025-05-18 23:20:39 -0400

    Refactor Engine class: streamline torque and power calculations, improve RPM validation, and enhance report output

--- classes/dragster/engine.js
+++ classes/dragster/engine.js
@@ -5,8 +5,7 @@ export default class Engine {
     #rpm_max;
     #rpm_min;
     #torque_max;
-    #torque_min;
-
+    #power_max;
 
     constructor(
         torque_curve = [
@@ -34,36 +33,42 @@ export default class Engine {
         this.#rpm_min = torque_curve[0][0];
         this.#rpm_max = torque_curve[torque_curve.length - 1][0];
 
-        // find the max torque
-        this.#torque_max = torque_curve[0][1];
-        for (let i = 1; i < torque_curve.length; i++) {
-            this.#torque_max = Math.max(this.#torque_max, torque_curve[i][1]);
-        }
+        this.#torque_max = 0;
+        this.#power_max = 0;
 
+        for (const [rpm, torque] of this.#torque_curve) {
+            this.#torque_max = Math.max(this.#torque_max, torque);
+            this.#power_max = Math.max(this.#power_max, torque * rpm / 5252);
+        }
     }
 
 
     #valid_rpm(rpm) {
-        return (rpm < this.#rpm_min || rpm > this.#rpm_max);
+        if (rpm < this.#rpm_min || rpm > this.#rpm_max) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     torque(rpm) {
         if (this.#valid_rpm(rpm)) {
-            return 0;
-        }
+            for (let i = 0; i < this.#torque_curve.length - 1; i++) {
+                if (rpm >= this.#torque_curve[i][0] && rpm <= this.#torque_curve[i + 1][0]) {
+                    const x1 = this.#torque_curve[i][0];
+                    const y1 = this.#torque_curve[i][1];
+                    const x2 = this.#torque_curve[i + 1][0];
+                    const y2 = this.#torque_curve[i + 1][1];
 
-        // find the torque at the given rpm
-        for (let i = 0; i < this.#torque_curve.length - 1; i++) {
-            if (rpm >= this.#torque_curve[i][0] && rpm <= this.#torque_curve[i + 1][0]) {
-                const x1 = this.#torque_curve[i][0];
-                const y1 = this.#torque_curve[i][1];
-                const x2 = this.#torque_curve[i + 1][0];
-                const y2 = this.#torque_curve[i + 1][1];
-
-                // linear interpolation
-                return y1 + (y2 - y1) * ((rpm - x1) / (x2 - x1));
+                    // linear interpolation
+                    return y1 + (y2 - y1) * ((rpm - x1) / (x2 - x1));
+                }
             }
+        } else {
+            console.log("ERROR: RPM out of range");
         }
+        console.log("RPM out of range");
+
         return 0;
     }
 
@@ -75,14 +80,15 @@ export default class Engine {
     report() {
         console.log("Engine report:");
         console.log("Max Torque: " + this.#torque_max);
+        console.log("Max Power: " + this.#power_max);
         console.log("Max RPM: " + this.#rpm_max);
         console.log("Min RPM: " + this.#rpm_min);
         console.log("Shift RPM: " + this.#rpm_shift);
         console.log("Launch RPM: " + this.#rpm_launch);
         console.log("Torque Curve: ");
         for (const [rpm, torque] of this.#torque_curve) {
-            power = this.power(rpm);
-            console.log(`${rpm}  RPM : ${torque} Ft-lb : ${power} HP`);
+            const hp = this.power(rpm);
+            console.log(`${rpm}  RPM : ${torque} Ft-lb : ${hp} HP`);
         }
     }
 }
