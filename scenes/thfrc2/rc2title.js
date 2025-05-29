394fa1b47bf815b474de3895cacd1ae7005de5b1
Jared De Blander <jared0x90@gmail.com>
2025-05-29 12:15:31 -0400

    Add RC2Title scene for "The Hunt for Roy Carnassus II" game

---
+++ scenes/thfrc2/rc2title.js
@@ -0,0 +1,33 @@
+/*
+
+The Hunt for Roy Carnassus II
+
+Title Screen
+
+
+
+
+
+*/
+
+export default class RC2Title extends Phaser.Scene {
+    constructor() {
+        super('RC2Title');
+    }
+
+    preload() {
+        // Load any assets if needed
+    }
+
+    create() {
+        const { width, height } = this.scale;
+        this.match_text = this.add.text(width / 2, height / 2, 'The Hunt for Roy Carnassus II', {
+            fontSize: '32px',
+            color: '#fff',
+        }).setOrigin(0.5);
+
+        this.input.once('pointerdown', () => {
+            this.scene.start('RC2Title');
+        });
+    }
+}
